39ab68b99c4dcbce90868ce858b5ff2a9aa5da7c
FasTrss <greyhollmen@gmail.com>
2022-11-19 09:53:16 +0300

    add assert to capitalize.test.js

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -2,13 +2,9 @@
 // попробуйте изменять код функции / тестов, запуская проверки заново
 
 import { capitalize } from '../src/capitalize.js';
+import { strict as assert } from 'node:assert';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
+assert.equal(capitalize('hello'), 'Hello')  
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+assert.equal(capitalize(''), '')
 
-console.log('Все тесты пройдены!');
